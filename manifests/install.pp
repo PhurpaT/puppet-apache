e64a5756120dfcb874c440b2ea405fd3158ad58a
root <root@87c6ee62ce1c.mylabserver.com>
2020-12-29 17:13:24 +0000

    restructred  module  using Heira

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,12 +4,9 @@
 #
 # @example
 #   include apache::install
-class apache::install (
-  $install_name = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
+class apache::install {
 
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
