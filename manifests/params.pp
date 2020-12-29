0a2a21cd199e636515dbd6372e28a9063e5fc8fa
root <root@87c6ee62ce1c.mylabserver.com>
2020-12-29 00:15:04 +0000

    restructred install class for Debian based OS using params

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+# @summary A short summary of the purpose of this class
+# OS related variables.
+#   include apache::params
+
+class apache::params {
+  $install_ensure = 'present'
+
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+ }
+    'Debian': {
+      $install_name = 'apache2'
+  } 
+ }
+}
